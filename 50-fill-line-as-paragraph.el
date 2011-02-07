6c8555e5d5a744d5fa4f1d2f3a04293a0ce225b5
David Lindes <misc-git-user@daveltd.com>
2011-02-07 02:47:32 -0800

    fill-line-as-paragraph, bound to C-M-l.

---
+++ 50-fill-line-as-paragraph.el
@@ -0,0 +1,9 @@
+(defun fill-line-as-paragraph()
+  "Fill the current line [TODO: or the next n lines, with arg] as a paragraph."
+  (interactive)
+  (fill-region-as-paragraph (line-beginning-position) (line-end-position)))
+
+; comment this out if you don't want the key binding:
+(global-set-key "\C-\M-l" 'fill-line-as-paragraph)
+
+; this is a test of a paragraph line to test this thing on.  It should be long enough that it needs to wrap.
